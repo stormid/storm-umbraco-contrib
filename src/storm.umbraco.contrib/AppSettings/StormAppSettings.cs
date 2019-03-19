a69b6e050d13f89b53cd40efcfc3a55e4e564282
Owen <Owen.Roberts@stormid.com>
2019-03-19 09:50:36 +0000

    Added abstract StormAppSettings class

---
+++ src/storm.umbraco.contrib/AppSettings/StormAppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Configuration;
+
+namespace storm.umbraco.contrib.AppSettings
+{
+    public abstract class StormAppSettings
+    {
+        protected static T GetAppSetting<T>(string name, T defaultValue)
+        {
+            try
+            {
+                return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(ConfigurationManager.AppSettings[name]);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
