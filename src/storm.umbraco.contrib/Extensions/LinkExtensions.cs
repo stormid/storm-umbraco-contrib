60e31ab8cae307cb923bf6d03b9fad598558da9b
James Hamilton <james.hamilton@stormid.com>
2019-03-28 10:21:51 +0000

    Add extension method to ensure that rel="noopener" is added when target="_blank"

---
+++ src/storm.umbraco.contrib/Extensions/LinkExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace storm.umbraco.contrib.Extensions
+{
+    public static class LinkExtensions
+    {
+        public static IHtmlString LinkTarget(this string value)
+        {
+            return (!string.IsNullOrEmpty(value) && value == "_blank") ? new HtmlString(@" target=""_blank"" rel=""noopener""") : new HtmlString(string.Empty);
+        }
+    }
+}
