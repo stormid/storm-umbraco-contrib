60868ee94dd16388d9b669a1f6c6267016fe3cc1
Owen <Owen.Roberts@stormid.com>
2019-03-29 12:16:21 +0000

    Added H1PageTitle extension

---
+++ src/storm.umbraco.contrib/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace storm.umbraco.contrib.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static string H1PageTitle(this IPublishedContent content)
+        {
+            var h1PageTitle = content.GetPropertyValue<string>(nameof(H1PageTitle));
+
+            return string.IsNullOrEmpty(h1PageTitle) ? content.Name : h1PageTitle;
+        }
+    }
+}
