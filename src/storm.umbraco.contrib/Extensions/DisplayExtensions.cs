19a18c28349e4fc503af3f3b692e511387dd4b84
Owen <Owen.Roberts@stormid.com>
2019-03-29 12:26:28 +0000

    Added string truncate method to display extensions

--- src/storm.umbraco.contrib/Extensions/DisplayExtensions.cs
+++ src/storm.umbraco.contrib/Extensions/DisplayExtensions.cs
@@ -8,5 +8,21 @@ namespace storm.umbraco.contrib.Extensions
         {
             return !string.IsNullOrEmpty(value) ? new HtmlString(value.Replace("\n", "<br />")) : new HtmlString(string.Empty);
         }
+
+        public static string Truncate(this string value, int length, string ellipsis, bool keepFullWordAtEnd)
+        {
+            if (string.IsNullOrEmpty(value)) return string.Empty;
+
+            if (value.Length < length) return value;
+
+            value = value.Substring(0, length);
+
+            if (keepFullWordAtEnd)
+            {
+                value = value.Substring(0, value.LastIndexOf(' '));
+            }
+
+            return value + ellipsis;
+        }
     }
 }
