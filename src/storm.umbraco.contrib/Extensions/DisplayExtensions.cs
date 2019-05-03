c38ca6a78e962c9f899d53c367b1881c230cc1ba
Owen <Owen.Roberts@stormid.com>
2019-05-03 14:29:33 +0100

    Added documentation

--- src/storm.umbraco.contrib/Extensions/DisplayExtensions.cs
+++ src/storm.umbraco.contrib/Extensions/DisplayExtensions.cs
@@ -9,6 +9,11 @@ namespace storm.umbraco.contrib.Extensions
             return !string.IsNullOrEmpty(value) ? new HtmlString(value.Replace("\n", "<br />")) : new HtmlString(string.Empty);
         }
 
+        /// <param name="value">The string to be trimmed.</param>
+        /// <param name="length">The character length the string will be shortened to.</param>
+        /// <param name="keepFullWordAtEnd">Keep the last full word, even if it goes over the maximum length.</param>
+        /// <param name="ellipsis">An optional additional string which will be appended at the end of the truncated string.</param>
+        /// <returns>The original string shortened to a specific character length.</returns>
         public static string Truncate(this string value, int length, bool keepFullWordAtEnd = true, string ellipsis = "...")
         {
             if (string.IsNullOrEmpty(value)) return string.Empty;
