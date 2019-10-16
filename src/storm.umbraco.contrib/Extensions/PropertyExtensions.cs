8a0686b6431d7179183e555365542d05f3d725a6
Owen <Owen.Roberts@stormid.com>
2019-10-16 12:39:18 +0100

    Add WithFallback extension method

--- src/storm.umbraco.contrib/Extensions/PropertyExtensions.cs
+++ src/storm.umbraco.contrib/Extensions/PropertyExtensions.cs
@@ -7,6 +7,30 @@ namespace storm.umbraco.contrib.Extensions
     public static class PropertyExtensions
     {
         /// <summary>
+        /// A method which attempts to returned a target property if it has a value. If not, a fallback property is returned. If both target and the fallback have no value, then a default value is returned.
+        /// </summary>
+        /// <typeparam name="T">The type of property to be returned.</typeparam>
+        /// <param name="targetProperty">The preferred property to be returned.</param>
+        /// <param name="fallbackProperty">A fallback property to be used if the targetProperty is not set.</param>
+        /// <param name="defaultValue">The default value to be returned if both the target and fallback properties are not set.</param>
+        /// <param name="treatDefaultAsNull">Should default values be returned.</param>
+        /// <returns>The targetProperty if it is set, the highest priority fallback property if it is set, or a default value.</returns>
+        public static T WithFallback<T>(this T targetProperty, T fallbackProperty = default, T defaultValue = default, bool treatDefaultAsNull = false)
+        {
+            if (targetProperty.IsPropertyNotNullOrEmpty(treatDefaultAsNull))
+            {
+                return targetProperty;
+            }
+
+            if (fallbackProperty.IsPropertyNotNullOrEmpty(treatDefaultAsNull))
+            {
+                return fallbackProperty;
+            }
+
+            return EqualityComparer<T>.Default.Equals(defaultValue, default) ? default : defaultValue;
+        }
+
+        /// <summary>
         /// A method which attempts to returned a target property if it has a value. If not, fallback properties are tried in order and the first property with a value is returned. If both target and fallback properties have no value, then a default value is returned.
         /// </summary>
         /// <typeparam name="T">The type of property to be returned.</typeparam>
